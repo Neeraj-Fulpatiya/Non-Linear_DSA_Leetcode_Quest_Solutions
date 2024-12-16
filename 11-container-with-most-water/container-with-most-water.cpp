8d6b25ed628f40abf9b7038d62963b9deef179a8
Neeraj Fulpatiya <128641556+Neeraj-Fulpatiya@users.noreply.github.com>
2024-12-16 08:07:30 +0530

    Time: 1 ms (45.84%) | Memory: 62.9 MB (17.93%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -3,8 +3,7 @@ public:
     int maxArea(vector<int>& height) {
        int left=0;
        int right=height.size()-1; 
-    int size=height.size();
-    int area=0;
+     int area=0;
 
     int width=height.size()-1;
 

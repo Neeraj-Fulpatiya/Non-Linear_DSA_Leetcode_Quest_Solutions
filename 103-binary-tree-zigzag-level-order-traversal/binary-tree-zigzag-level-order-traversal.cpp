058627414441f4004271883e82b0f63975e5c589
Neeraj Fulpatiya <128641556+Neeraj-Fulpatiya@users.noreply.github.com>
2025-01-07 09:30:01 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (33.96%) - LeetSync

---
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if (!root) return ans;
+        deque<TreeNode*> dq;
+        dq.push_back(root);
+        bool lefttoright = true;
+
+        while (!dq.empty()) {
+            int len = dq.size();
+            vector<int> vec;
+            while (len--){
+                if (!lefttoright) {
+                    root = dq.back();
+                    dq.pop_back();
+                    vec.push_back(root->val);
+                    if (root->right) dq.push_front(root->right);
+                    if (root->left) dq.push_front(root->left);
+                }
+                else {
+                    root = dq.front();
+                    dq.pop_front();
+                    vec.push_back(root->val);
+                    if (root->left) dq.push_back(root->left);
+                    if (root->right) dq.push_back(root->right);
+                }
+            }
+            ans.push_back(vec);
+            lefttoright = !lefttoright;
+        }
+        return ans;
+    }
+};

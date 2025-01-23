5148ecda5cee528d8102383f917d461a38d13ef7
Neeraj Fulpatiya <128641556+Neeraj-Fulpatiya@users.noreply.github.com>
2025-01-23 11:19:39 +0530

    Time: 5 ms (53.86%) | Memory: 15.8 MB (9.83%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // unordered_map<int,int>m;
-        // vector<int>ans;
-        // for(int i=0;i<nums.size();i++){
-        //     m[nums[i]]=i;
-        // }
+        unordered_map<int,int>m;
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]=i;
+        }
         // for(auto it:nums){
-        //     if(m.find(target-it)!=m.end()){
-        //         return {it.}
-        //     }
-        // }
-        int n=nums.size();
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-           if(nums[i]+nums[j]==target)return {i,j};
+            for(int i=0;i<nums.size();i++){
+
+            if(m.find(target-nums[i])!=m.end()){
+int sec=m.find(target-nums[i])->second;
+// int ind=sec.second;
+if(sec!=i)
+                {return {i,sec};}
             }
         }
+        // int n=nums.size();
+        // for(int i=0;i<n-1;i++){
+        //     for(int j=i+1;j<n;j++){
+        //    if(nums[i]+nums[j]==target)return {i,j};
+        //     }
+        // }
         return {};
     }
 };
